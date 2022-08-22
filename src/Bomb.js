885a9b772e4ac54809c71a04aeec200c02f44e1f
Nick <nelom3@gmail.com>
2022-08-22 10:07:32 -0400

    commit tree 73655b07b289ffc7e1ca73ed6c12cfb29ea5790c

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,21 @@
+import { Component } from "react";
+
 // your Bomb code here!
+export default class Bomb extends Component {
+
+  constructor(props) {
+    super() 
+    debugger
+    this.state = {secondsLeft: props.initialCount}
+  }
+
+  render() {
+    if (this.state.secondsLeft === 0) {
+      return "Boom!"
+    } else {
+      return `${this.state.secondsLeft} seconds left before I go boom!`
+    }
+
+  }
+
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,15 @@
+import { Component } from "react";
+
 // your ImageSlider code here!
+export default class Slider extends Component {
+  
+  constructor() {
+    super();
+    this.state = {currentSlideIndex: 0}
+  }
+
+  render() {
+    return `I am on slide ${this.state.currentSlideIndex}`
+  }
+
+}
